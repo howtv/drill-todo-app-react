210fd5a071c9906d8a36b960836e75268826f3bf
k7ubb <kubota.sou@vega.ocn.ne.jp>
2024-11-20 12:42:20 +0900

    STEP4-1: タスクの内容をLocalStorageに保持

--- src/useHandleTasks.ts
+++ src/useHandleTasks.ts
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export type Task = {
   title: string;
@@ -8,27 +8,44 @@ export type Task = {
 };
 
 export const useHandleTasks = () => {
-  const [tasks, setTasks] = useState<Task[]>([
-    {
-      title: "買い物",
-      done: true,
-      category: "生活",
-    },
-    {
-      title: "メール返信",
-      done: false,
-      category: "仕事",
-    },
-    {
-      title: "レポート提出",
-      done: false,
-      category: "仕事",
-    },
-  ]);
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [loaded, setIsLoaded] = useState(false);
 
   const addTask = (task: Task) => {
     setTasks([...tasks, task]);
   };
+
+  useEffect(() => {
+    const tasksJson = localStorage.getItem("tasks");
+    if (tasksJson) {
+      setTasks(JSON.parse(tasksJson));
+    } else {
+      setTasks([
+        {
+          title: "買い物",
+          done: true,
+          category: "生活",
+        },
+        {
+          title: "メール返信",
+          done: false,
+          category: "仕事",
+        },
+        {
+          title: "レポート提出",
+          done: false,
+          category: "仕事",
+        },
+      ]);
+    }
+    setIsLoaded(true);
+  }, []);
+
+  useEffect(() => {
+    if (loaded) {
+      localStorage.setItem("tasks", JSON.stringify(tasks));
+    }
+  }, [loaded, tasks]);
 
   const removeTask = (task: Task) => {
     setTasks(tasks.filter((_) => _ !== task));
