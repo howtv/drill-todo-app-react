fc5e7a8158fc568b215a094a7ee1679e08f7acda
k7ubb <kubota.sou@vega.ocn.ne.jp>
2024-11-20 12:29:21 +0900

    STEP1: 最低限の機能を実装

---
+++ src/useHandleTasks.ts
@@ -0,0 +1,51 @@
+import { useState } from "react";
+
+export type Task = {
+  title: string;
+  done: boolean;
+};
+
+export const useHandleTasks = () => {
+  const [tasks, setTasks] = useState<Task[]>([
+    {
+      title: "買い物",
+      done: true,
+    },
+    {
+      title: "メール返信",
+      done: false,
+    },
+    {
+      title: "レポート提出",
+      done: false,
+    },
+  ]);
+
+  const addTask = (task: Task) => {
+    setTasks([...tasks, task]);
+  };
+
+  const removeTask = (task: Task) => {
+    setTasks(tasks.filter((_) => _ !== task));
+  };
+
+  const setTaskDone = (task: Task, done: boolean) => {
+    setTasks(
+      tasks.map((_) =>
+        _ !== task
+          ? _
+          : {
+              ...task,
+              done,
+            }
+      )
+    );
+  };
+
+  return {
+    tasks,
+    addTask,
+    removeTask,
+    setTaskDone,
+  };
+};
